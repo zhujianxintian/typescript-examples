b20578138335213f7912ba6b4776d29179516c8a
ldp <1901967098@qq.com>
2024-06-28 11:11:20 +0800

    使用 typescript 编写 node esm 标准代码

---
+++ packages/node-typescript-esm/src/index.ts
@@ -0,0 +1,3 @@
+import { formatDateTime } from './utils/format-date-time';
+
+console.log('node-esm', formatDateTime(new Date().toISOString()));
---
+++ packages/node-typescript-esm/src/utils/format-date-time.ts
@@ -0,0 +1,14 @@
+export const zeroFill = (number: number) => {
+    return number >= 10 ? number : `0${number}`;
+};
+
+export const formatDateTime = (value: string) => {
+    const date = new Date(value);
+    const year = date.getFullYear();
+    const month = zeroFill(date.getMonth() + 1);
+    const day = zeroFill(date.getDate());
+    const hours = zeroFill(date.getHours());
+    const minutes = zeroFill(date.getMinutes());
+    const seconds = zeroFill(date.getSeconds());
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
